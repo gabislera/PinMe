651e77ccc9f75997b6a9692b673a6af50f405fce
Gabriela <gabriela.carniel@sellflux.com.br>
2025-05-16 20:55:40 -0300

    feat: auth pages

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,15 +1,31 @@
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
+
 export const SignIn = () => {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <div>
-                <h1>Log in to your account</h1>
-                <p>Welcome back! Please enter your details.</p>
-                <form className="flex flex-col gap-4">
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <button type="submit">Log in</button>
-                </form>
-            </div>
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-gray-800 via-gray-900 to-gray-950 ">
+      <div className="flex flex-col items-center gap-4 w-full max-w-[450px] bg-white/10 backdrop-blur-md border border-white/20 p-8 rounded-3xl shadow-lg">
+        <div className="flex flex-col items-center gap-1">
+          <h1 className="text-3xl font-bold text-white">Acesse sua conta</h1>
+          <p className="text-muted text-center">Informe seus dados para continuar.</p>
         </div>
-    )
-}
+
+        <form className="flex flex-col gap-2 w-full mt-4">
+          <Input label="Email" placeholder="exemplo@email.com" type="email" />
+          <Input label="Password" placeholder="********" type="password" />
+          <Button title="Entrar" type="submit" className="mt-4" />
+        </form>
+
+        <span className="text-muted text-sm text-center">
+          Ainda não tem uma conta?{' '}
+          <a
+            href="/register"
+            className="text-tertiary hover:text-secondary transition-colors duration-300 font-bold"
+          >
+            Crie agora
+          </a>
+        </span>
+      </div>
+    </div>
+  );
+};
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,7 +1,33 @@
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
+
 export const SignUp = () => {
   return (
-    <div>
-      <h1>SignUp</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-gray-800 via-gray-900 to-gray-950 ">
+      <div className="flex flex-col items-center gap-4 w-full max-w-[450px] bg-white/10 backdrop-blur-md border border-white/20 p-8 rounded-3xl shadow-lg">
+        <div className="flex flex-col items-center gap-1">
+          <h1 className="text-3xl font-bold text-white">Crie sua conta</h1>
+          <p className="text-muted text-center">Informe seus dados para continuar.</p>
+        </div>
+
+        <form className="flex flex-col gap-2 w-full mt-4">
+          <Input label="Nome" placeholder="exemplo@email.com" type="email" />
+          <Input label="Email" placeholder="exemplo@email.com" type="email" />
+          <Input label="Senha" placeholder="********" type="password" />
+          <Input label="Confirmar senha" placeholder="********" type="password" />
+          <Button title="Cadastrar" type="submit" className="mt-4" />
+        </form>
+
+        <span className="text-muted text-sm text-center">
+          Já tem uma conta?{' '}
+          <a
+            href="/"
+            className="text-tertiary hover:text-secondary transition-colors duration-300 font-bold"
+          >
+            Acesse sua conta
+          </a>
+        </span>
+      </div>
     </div>
   );
 };
