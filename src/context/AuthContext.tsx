a40abca824df20ac9b4570dfbbd4b4edf03e3138
Gabriela <gabriela.carniel@sellflux.com.br>
2025-05-17 13:00:36 -0300

    feat: authentication context and hook

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, useState, type ReactNode } from 'react';
+import * as authService from '../services/auth/authService';
+import type { SignUpSchema } from '../pages/auth/SignUp';
+import type { SignInSchema } from '../pages/auth/SignIn';
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface AuthContextProps {
+  isAuthenticated: boolean;
+  signUp: (data: SignUpSchema) => void;
+  signIn: (data: SignInSchema) => void;
+}
+
+export const AuthContext = createContext({} as AuthContextProps);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  function signUp(data: SignUpSchema) {
+    authService.signUp(data);
+    setIsAuthenticated(true);
+  }
+
+  function signIn(data: SignInSchema) {
+    authService.signIn(data);
+    setIsAuthenticated(true);
+  }
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, signUp, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
